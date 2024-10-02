f8822e7ed4d5c3747d7fcac718e78b4337630415
SamikshaaSharma <samiksha870760@gmail.com>
2024-10-02 13:15:31 +0530

    changes in linearsearch

--- linearsearch.java
+++ linearsearch.java
@@ -1,12 +1,23 @@
-public class linearsearch{
-    public static int main(String[] args) {
-        int x = 10; 
-        int arr[] = {1,2,4,10};
-        for(int i = 0; i < arr.length; i++){
-            if(x == arr[i]){
-               System.out.println(i);
+public class linearsearch
+ {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 1, 7, 5};
+        int x = 4; // Element to search for
+        int index = -1; // Default value if element is not found
+
+        // Linear search logic
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                index = i;
+                break;
             }
         }
-        return -1;
+
+        // Output the result
+        if (index == -1) {
+            System.out.println("Element is not present in the array");
+        } else {
+            System.out.println("Element found at position " + index);
+        }
     }
 }
